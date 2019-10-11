d80daf0acda6bc55546fa657da8c89ba303214be
Danny <windate3411@gmail.com>
2019-10-12 01:36:08 +0800

    modify test case

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       // assert.equal([1, 2, 3].indexOf(4), -1); 註解此行 
-      expect([1, 2, 3].includes(3)).to.be.true //加入這行，並動點手腳讓測試失敗
+      expect([1, 2, 4].includes(3)).to.be.true //加入這行，並動點手腳讓測試失敗
     });
   });
 })
